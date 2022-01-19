2760e632ae07cfb7d23ce2dc9a0c0e130eb1120d
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-01-19 12:45:28 +0100

    CRUD user

---
+++ src/Controller/Front/UserController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+
+    /**
+     * @Route("user/insert/", name="user_insert")
+     */
+    public function insertUser(
+        Request $request,
+        EntityManagerInterface $entityManagerInterface,
+        UserPasswordHasherInterface $userPasswordHasherInterface
+    ) {
+
+        $user = new User();
+
+        $userForm = $this->createForm(UserType::class, $user);
+
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $user->setRoles(["ROLE_USER"]);
+
+            $plainPassword = $userForm->get('password')->getData();
+
+            $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $plainPassword);
+
+            $user->setPassword($hashedPassword);
+
+            $entityManagerInterface->persist($user);
+
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render("front/userform.html.twig", ['userForm' => $userForm->createView()]);
+    }
+
+    /**
+     * @Route("update/user", name="user_update")
+     */
+    public function userUpdate(
+        Request $request,
+        EntityManagerInterface $entityManagerInterface,
+        UserPasswordHasherInterface $userPasswordHasherInterface,
+        UserRepository $userRepository
+    ) {
+
+        // récupère le user connecté
+        $user_connect = $this->getUser();
+
+        $user_mail =  $user_connect->getUserIdentifier();
+
+        $user = $userRepository->findOneBy(['email' => $user_mail]);
+
+        $userForm = $this->createForm(UserType::class, $user);
+
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+
+            $plainPassword = $userForm->get('password')->getData();
+
+            $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $plainPassword);
+
+            $user->setPassword($hashedPassword);
+
+            $entityManagerInterface->persist($user);
+
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render("front/userform.html.twig", ['userForm' => $userForm->createView()]);
+    }
+
+    /**
+     * @Route("delete/user", name="delete_user")
+     */
+    public function deleteUser(UserRepository $userRepository, EntityManagerInterface $entityManagerInterface)
+    {
+        $user_connect = $this->getUser();
+
+        $user_mail = $user_connect->getUserIdentifier();
+
+        $user = $userRepository->findOneBy(['email' => $user_mail]);
+
+        $entityManagerInterface->remove($user);
+
+        $entityManagerInterface->flush();
+
+        return $this->redirectToRoute('article_list');
+    }
+}
