f932aba3a9c1b04804a36251b5224358a1987057
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-01-19 14:13:27 +0100

    READ user

---
+++ src/Controller/Admin/AdminUserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminUserController extends AbstractController
+{
+
+    /**
+     * @Route("admin/users", name="user_list")
+     */
+    public function userList(UserRepository $userRepository)
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render("admin/users.html.twig", ['users' => $users]);
+    }
+}
